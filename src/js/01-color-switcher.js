5c328601ff25ddfb3168878f22cd2f1b52fe21c1
kolesnva <kolesnychenko_v@icloud.com>
2022-06-15 18:23:35 +0300

    task-01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const refs = {
+  startButton: document.querySelector('button[data-start]'),
+  stopButton: document.querySelector('button[data-stop]')
+};
 
+const DELAY = 1000;
+let switchInterval = 0;
+const btnNotActive = true;
+
+refs.startButton.addEventListener('click', switchStart);
+refs.stopButton.addEventListener('click', switchStop);
+
+refs.stopButton.disabled = btnNotActive;
+
+function switchStart() {
+  refs.startButton.disabled = btnNotActive;
+  refs.stopButton.disabled = !btnNotActive;
+
+  colorSwitcher();
+
+  switchInterval = setInterval(colorSwitcher, DELAY);
+};
+
+function colorSwitcher() {
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+
+function switchStop() {
+  refs.startButton.disabled = !btnNotActive;
+  refs.stopButton.disabled = btnNotActive;
+
+  clearInterval(switchInterval);
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
